1175af23ab2340847997bd480867b6a4e03dea68
HaydeRose <103533698+HaydeRose@users.noreply.github.com>
2022-05-12 17:55:21 -0400

    lintered

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,9 @@
-console.log("Hello/Hola, Welcome to my Portfolio")
-
-
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+console.log('Hello/Hola, Welcome to my Portfolio')
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
-} 
+}
