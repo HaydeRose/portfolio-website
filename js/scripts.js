e11786ce93ad6276d81dfd6f1f1902c7f70bfb91
HaydeRose <103533698+HaydeRose@users.noreply.github.com>
2022-05-31 12:53:22 -0400

    fixed nav menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,1 @@
 console.log('Hello/Hola, Welcome to my Portfolio')
-function menuToggle () {
-  var x = document.getElementById('myNavtoggle')
-  if (x.className === 'navtoggle') {
-    x.className += ' responsive'
-  } else {
-    x.className = 'navtoggle'
-  }
-}
